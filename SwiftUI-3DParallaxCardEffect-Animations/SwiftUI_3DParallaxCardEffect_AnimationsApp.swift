1d5761db65cfdc13b9d7b879862ca76b6987e240
alaxhenry <alaxhenry0121@gmail.com>
2022-12-15 12:57:46 +0900

    Initial Commit

---
+++ SwiftUI-3DParallaxCardEffect-Animations/SwiftUI_3DParallaxCardEffect_AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_3DParallaxCardEffect_AnimationsApp.swift
+//  SwiftUI-3DParallaxCardEffect-Animations
+//
+//  Created by Seungchul Ha on 2022/12/15.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_3DParallaxCardEffect_AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
